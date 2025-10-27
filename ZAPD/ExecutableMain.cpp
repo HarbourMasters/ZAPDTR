a5db91eb61429cea329aecb1b6165aa23ef7932b
Malkierian <malkierian@gmail.com>
2025-10-27 12:37:40 -0700

    `int main` doesn't need pointer parameters, since that only gets called on standalone.

--- ZAPD/ExecutableMain.cpp
+++ ZAPD/ExecutableMain.cpp
@@ -3,6 +3,6 @@
 extern "C" int zapd_main(int argc, char* argv[]);
 extern "C" int zapd_report(int argc, char* argv[], size_t* extractCount, size_t* totalExtract);
 
-int main(int argc, char* argv[], size_t* extractCount = nullptr, size_t* totalExtract = nullptr) {
-    return zapd_report(argc, argv, extractCount, totalExtract);
+int main(int argc, char* argv[]) {
+    return zapd_report(argc, argv, nullptr, nullptr);
 }
